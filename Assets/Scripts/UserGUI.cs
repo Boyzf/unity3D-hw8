d615ed74ff73033b7bf369a327945e0188bf4415
Boyzf <541153850@qq.com>
2020-01-04 11:46:43 +0800

    hw8

---
+++ Assets/Scripts/UserGUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserGUI : MonoBehaviour
+{
+    private GameObject firework;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firework = Instantiate(Resources.Load<GameObject>("Prefabs/FireworkGreen"), Vector3.zero, Quaternion.identity);
+    }
+
+}
